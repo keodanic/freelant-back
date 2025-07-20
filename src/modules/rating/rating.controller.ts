2481778b1a1f91920aebd81e1bb6be589741e9cd
keodanic <victordanoelsc14@gmail.com>
2025-07-19 22:42:26 -0300

    feat:Atualizações de Rating

--- src/modules/rating/rating.controller.ts
+++ src/modules/rating/rating.controller.ts
@@ -6,13 +6,17 @@ import {
   HttpStatus,
   Get,
   Param,
+  NotFoundException,
 } from '@nestjs/common';
 import { RatingService } from './rating.service';
 import { CreateRatingDto } from './dto/create-rating.dto';
+import { FreelancersService } from '../freelancers/freelancers.service';
 
 @Controller('ratings')
 export class RatingController {
-  constructor(private readonly ratingService: RatingService) {}
+  constructor(private readonly ratingService: RatingService,
+    private readonly freelancersService: FreelancersService
+  ) {}
 
   /**
    * POST /ratings
@@ -40,6 +44,21 @@ export class RatingController {
       return await this.ratingService.getRatingsByFreelancer(freelancerId);
     } catch (err) {
       throw new HttpException(err.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  // Adicione esta rota dentro da classe FreelancersController
+
+  @Get(':id/average-rating')
+  async getAverageRating(@Param('id') id: string) {
+    try {
+      const average = await this.freelancersService.getAverageRating(id);
+      return { average }; // Retorna exatamente no formato { average: number }
+    } catch (err) {
+      if (err instanceof NotFoundException) {
+        throw new HttpException(err.message, HttpStatus.NOT_FOUND);
+      }
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
--- src/modules/rating/rating.module.ts
+++ src/modules/rating/rating.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/database/prisma.module';
 import { RatingController } from './rating.controller';
 import { RatingService } from './rating.service';
+import { FreelancersModule } from 'src/modules/freelancers/freelancers.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,FreelancersModule],
   controllers: [RatingController],
   providers: [RatingService],
   exports: [RatingService],
